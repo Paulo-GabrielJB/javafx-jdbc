c8cb8c61dc393f9e0e8a7b3c4a063e3e2c33d7a7
Paulo Gabriel <pgj1085@gmail.com>
2020-04-09 15:12:37 -0300

    Adicionando campos no SellerFormFiew

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -152,7 +152,7 @@ public class SellerDaoJDBC implements SellerDao {
 		seller.setId(rs.getInt("Id"));
 		seller.setName(rs.getString("Name"));
 		seller.setEmail(rs.getString("Email"));
-		seller.setBirthDate(rs.getDate("BirthDate"));
+		seller.setBirthDate(new java.util.Date(rs.getDate("BirthDate").getTime()));
 		seller.setBaseSalary(rs.getDouble("BaseSalary"));
 		seller.setDepartment(dep);
 		return seller;
