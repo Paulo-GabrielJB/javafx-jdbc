7a91a77f83a48e3c133ecfbc37fc410582dee207
Paulo Gabriel <pgj1085@gmail.com>
2020-04-09 09:30:18 -0300

    Passando um obj department para o departmentform

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -47,7 +47,8 @@ public class DepartmentListController implements Initializable {
 	public void onBtnNewAction(ActionEvent event) {
 		
 		Stage parentStage = gui.util.Utils.currentStage(event);
-		createDialogForm("/gui/DepartmentForm.fxml", parentStage);
+		Department obj = new Department();
+		createDialogForm(obj, "/gui/DepartmentForm.fxml", parentStage);
 		
 	}
 	
@@ -85,11 +86,16 @@ public class DepartmentListController implements Initializable {
 		tableViewDeparment.setItems(obsList);
 	}
 	
-	private void createDialogForm(String absoluteName, Stage parentStage) {
+	private void createDialogForm(Department obj, String absoluteName, Stage parentStage) {
 		try {
 			
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
 			Pane pane = loader.load();
+			
+			
+			DepartmentFormController controller = loader.getController(); //pega o controller instanciado no form
+			controller.setDepartment(obj);
+			controller.updateFormData();
 			
 			Stage dialogStage = new Stage();
 			
